f0758dfd2d4f4c054393a60c46acefd6f9188d9d
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-04-12 09:50:27 +0200

    init commit

---
+++ src/main/kotlin/it/frigir/msscbreweryclient/MsscBreweryClientApplication.kt
@@ -0,0 +1,11 @@
+package it.frigir.msscbreweryclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsscBreweryClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsscBreweryClientApplication>(*args)
+}
